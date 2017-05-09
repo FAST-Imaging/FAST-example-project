182703bc63d19b98bdff99b852a364f3b459984c
Erik Smistad <ersmistad@gmail.com>
2017-05-09 12:02:58 +0200

    Set up a simple pipeline

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
+#include <FAST/Importers/ImageFileImporter.hpp>
+#include <FAST/Visualization/ImageRenderer/ImageRenderer.hpp>
 #include <FAST/Visualization/SimpleWindow.hpp>
+
 
 using namespace fast;
 
 int main(int argc, char** argv) {
+    // Output info messages etc.
+    Reporter::setGlobalReportMethod(Reporter::COUT);
+
+    // Import FAST logo image
+    ImageFileImporter::pointer importer = ImageFileImporter::New();
+    importer->setFilename(Config::getDocumentationPath() + "images/FAST_logo_square.png");
+
+    // Set up renderer
+    ImageRenderer::pointer renderer = ImageRenderer::New();
+    renderer->addInputConnection(importer->getOutputPort());
+
+    // Set up window and start pipeline
     SimpleWindow::pointer window = SimpleWindow::New();
+    window->addRenderer(renderer);
     window->start();
 }
